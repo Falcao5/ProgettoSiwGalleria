61cde523a1d7d5b9821c6e9d016ad0dc9d956e40
francescolauzi <francesco.lauzi@gmail.com>
2017-06-09 20:44:59 +0200

    Create Utente.java

--- Galleria/src/main/java/it/uniroma3/spring/model/Utente.java
+++ Galleria/src/main/java/it/uniroma3/spring/model/Utente.java
@@ -8,10 +8,68 @@
 
 package it.uniroma3.spring.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="utenti")
+@NamedQueries({
+	@NamedQuery(name = "Utente.findAll", query = "SELECT u FROM Utente u"),
+	@NamedQuery(name = "Utente.findByUsername", query = "SELECT u FROM Utente u WHERE u.username = :username"),
+	@NamedQuery(name = "Utente.findById", query="SELECT u FROM Utente u WHERE u.id = :id")
+})
 @Entity
 public class Utente {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable=false, unique=true)
+	private String username;
+	
+	@Column(nullable=false)
+	private String password;
+	
+	public Utente(){
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return this.id+"";
+	}
+
 	
 }
