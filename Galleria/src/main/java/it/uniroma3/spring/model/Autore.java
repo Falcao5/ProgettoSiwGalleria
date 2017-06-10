7fb2b3819321e724ea0284639f1f67ec74f74940
falcao5 <marte.vfalcone@gmail.com>
2017-06-10 13:42:44 +0200

    Aggiunto nuovo ramo "Falcao5". Rimossa la classe astratta Opera in quanto sui requisiti c'è scritto che "per semplicità supponiamo che le opere d'arte siano quadri". Modificato di conseguenza la classe Quadro, ora ha un riferimento all'autore ManyToOne. Modificato Autore: la variabile nazionalità era scritta con l'accento, sempre meglio evitare di utilizzare gli accenti.

--- Galleria/src/main/java/it/uniroma3/spring/model/Autore.java
+++ Galleria/src/main/java/it/uniroma3/spring/model/Autore.java
@@ -30,7 +30,7 @@ import javax.persistence.TemporalType;
 	@NamedQuery(name="Autore.findById", query="SELECT a FROM Autore a WHERE a.id = :id"),
 	@NamedQuery(name="Autore.findByNome", query="SELECT a FROM Autore a WHERE a.nome = :nome"),
 	@NamedQuery(name="Autore.findByCognome", query="SELECT a FROM Autore a WHERE a.cognome = :cognome"),
-	@NamedQuery(name="Autore.findByNazionalità", query="SELECT a FROM Autore a WHERE a.nazionalità = :nazionalità"),
+	@NamedQuery(name="Autore.findByNazionalità", query="SELECT a FROM Autore a WHERE a.nazionalita = :nazionalita"),
 	@NamedQuery(name="Autore.findByDataDiNascita", query="SELECT a FROM Autore a WHERE a.dataDiNascita = :dataDiNascita"),
 	@NamedQuery(name="Autore.findByDataDiMorte", query="SELECT a FROM Autore a WHERE a.dataDiMorte = :dataDiMorte")
 })
