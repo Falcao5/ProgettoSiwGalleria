26c74308f4c84db9de380da882acce89a15a341e
falcao5 <marte.vfalcone@gmail.com>
2017-06-12 15:56:41 +0200

    Creata una classe di test di unità (Usando JUnit 4), funzionano i primi test sulla riflessione della classe AbstractFacade!!

---
+++ Galleria/src/test/java/it/uniroma3/spring/facade/AmministratoreFacadeTest.java
@@ -0,0 +1,42 @@
+package it.uniroma3.spring.facade;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AmministratoreFacadeTest {
+	
+	private AbstractFacade ammfac;
+	
+	@Before
+	public void setUp(){
+		this.ammfac = new AmministratoreFacade();
+
+	}
+	
+	@Test
+	public void trovaNomeDiQuestaClasseTest() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method temp = null;
+		
+		try {
+			temp = this.ammfac.getClass().getMethod("trovaNomeDiQuestaClasse");
+			temp.setAccessible(true);
+		} catch (NoSuchMethodException e) {
+			System.out.println("Non esiste il metodo cercato");
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			System.out.println("Violazione accessibility");
+			e.printStackTrace();
+		}
+		
+		assertEquals("Amministratore", temp.invoke(obj, args));
+	}
+	
+	
+	
+}
