f6a0fffba2f7eb43e65c6ce1b5856fd9f12a14d3
falcao5 <marte.vfalcone@gmail.com>
2017-06-13 12:14:11 +0200

    Aggiunto handling per registrare amministratori e utenti in MainController

--- Galleria/src/main/java/it/uniroma3/spring/controller/MainController.java
+++ Galleria/src/main/java/it/uniroma3/spring/controller/MainController.java
@@ -3,23 +3,43 @@ package it.uniroma3.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.uniroma3.spring.facade.AmministratoreFacade;
+import it.uniroma3.spring.facade.UtenteFacade;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
+	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login(){
 		return "login";
 	}
 	
-	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	@RequestMapping(value="/signup", method=RequestMethod.GET)
 	public String signup(){
 		return "registrati";
 	}
 	
-	@RequestMapping("/home")
+	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String goHome(){
 		return "index";
+	}
+	
+	@RequestMapping(value="/registraUtente", method=RequestMethod.POST)
+	public String registraUtente(@RequestParam("username") String username, 
+			@RequestParam("password") String password){
+		UtenteFacade uf = new UtenteFacade();
+		uf.createUtente(username, password);
+		return "login";
+	}
+	
+	@RequestMapping(value="/registraAmministratore", method=RequestMethod.POST)
+	public String registraAmministratore(@RequestParam("username") String username, 
+			@RequestParam("password") String password){
+		AmministratoreFacade af = new AmministratoreFacade();
+		af.createAmministratore(username, password);
+		return "login";
 	}
 	
 }
