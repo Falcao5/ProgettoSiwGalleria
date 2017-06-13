eb8d309bf15e4cc7be9e1fc7488fa5e460de4fd1
falcao5 <marte.vfalcone@gmail.com>
2017-06-13 09:17:16 +0200

    Risolto finalmente il problema della mappatura delle richieste!

--- Galleria/src/main/java/it/uniroma3/spring/controller/MainController.java
+++ Galleria/src/main/java/it/uniroma3/spring/controller/MainController.java
@@ -1,23 +1,36 @@
 package it.uniroma3.spring.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Controller
 public class MainController {
 	
-	@PostMapping("/login")
+	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(){
-		return "cazzo";
+		return "login";
 	}
 	
-	@PostMapping("/signup")
+	@RequestMapping(value="/signup", method=RequestMethod.POST)
 	public String signup(){
 		return "registrati";
 	}
 	
-	@PostMapping("/home")
+	@RequestMapping("/home")
 	public String goHome(){
 		return "index";
 	}
+	/*
+	@Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+    */
 }
