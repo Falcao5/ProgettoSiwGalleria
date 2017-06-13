c37b869e8694ef4a0bfd28c028fa4aaa6c0cc045
falcao5 <marte.vfalcone@gmail.com>
2017-06-13 14:06:55 +0200

    Refactoring del test di AmministratoreFacade utilizzando il pattern della riflessione per testare metodi privati

--- Galleria/src/test/java/it/uniroma3/spring/facade/AmministratoreFacadeTest.java
+++ Galleria/src/test/java/it/uniroma3/spring/facade/AmministratoreFacadeTest.java
@@ -8,34 +8,35 @@ import java.lang.reflect.Method;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.uniroma3.spring.model.Amministratore;
+
 
 public class AmministratoreFacadeTest {
 	
-	private AbstractFacade ammfac;
+	private AbstractFacade<Amministratore> ammfac;
 	
 	@Before
 	public void setUp(){
 		this.ammfac = new AmministratoreFacade();
-
 	}
 	
 	/* Utilizza la riflessione per invocare il metodo della classe. In questo modo possiamo lasciare privato il metodo. */
 	@Test
 	public void trovaNomeDiQuestaClasseTest() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		Method temp = null;
-		
 		try {
-			temp = this.ammfac.getClass().getMethod("trovaNomeDiQuestaClasse");
+			temp = this.ammfac.getClass().getSuperclass().getDeclaredMethod("trovaNomeDiQuestaClasse");
 			temp.setAccessible(true);
+			this.ammfac = new AmministratoreFacade();
 		} catch (NoSuchMethodException e) {
 			System.out.println("Non esiste il metodo cercato");
 			e.printStackTrace();
-		} catch (SecurityException e) {
+		} catch (SecurityException e){
 			System.out.println("Violazione accessibility");
 			e.printStackTrace();
 		}
-		
-		assertEquals("Amministratore", temp.invoke(obj, args));
+
+		assertEquals("Amministratore", temp.invoke(this.ammfac));
 	}
 	
 	
