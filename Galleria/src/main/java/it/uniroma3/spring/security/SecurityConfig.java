ee6bf6331191675bb02914a04a016b805ee3f91e
falcao5 <marte.vfalcone@gmail.com>
2017-06-14 12:22:55 +0200

    Risolto il problema su AuthenticationManager, ora il server riesce a partire.

--- Galleria/src/main/java/it/uniroma3/spring/security/SecurityConfig.java
+++ Galleria/src/main/java/it/uniroma3/spring/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package it.uniroma3.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,14 +44,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-        .authorizeRequests()
-            .anyRequest().authenticated()
+        	.authorizeRequests()
+            .anyRequest()
+            .authenticated()
             .and()
-        .formLogin()
+            .formLogin()
             .loginPage("/login")
             .permitAll()
             .and()
-        .logout()
-           .permitAll();
+            .logout()
+            .permitAll();
+    }
+    
+    @Bean 
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
